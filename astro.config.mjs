aa58cff2139da977d201f0727d2d28b43fdd2584
Nauris Pūķis <hi@pyronaur.com>
2022-03-06 16:16:43 +0200

    Add shiki

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,20 @@
 export default /** @type {import('astro').AstroUserConfig} */ ({
 	// Comment out "renderers: []" to enable Astro's default component support.
 	renderers: [],
+	markdownOptions: {
+		render: [
+			'@astrojs/markdown-remark',
+			{
+				// Pick a syntax highlighter. Can be 'prism' (default), 'shiki' or false to disable any highlighting.
+				syntaxHighlight: 'shiki',
+				// If you are using shiki, here you can define a global theme and
+				// add custom languages.
+				shikiConfig: {
+					theme: 'github-dark',
+					langs: [],
+					wrap: false,
+				},
+			},
+		],
+	},
 });
