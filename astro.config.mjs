540bee88c814b435e89789105a47ea9cce147062
Nauris Pūķis <hi@pyronaur.com>
2022-03-30 17:40:37 +0300

    `syntaxHighlight: shiki` no longer necessary

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,10 +12,6 @@ export default /** @type {import('astro').AstroUserConfig} */ ({
 					['rehype-autolink-headings', { behavior: 'wrap' }],
 				],
 
-				// Pick a syntax highlighter. Can be 'prism' (default), 'shiki' or false to disable any highlighting.
-				syntaxHighlight: 'shiki',
-				// If you are using shiki, here you can define a global theme and
-				// add custom languages.
 				shikiConfig: {
 					theme: 'poimandres',
 					langs: [],
