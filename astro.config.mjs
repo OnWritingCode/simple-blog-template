9fe2fd19ff50f31a2d54ac353f763620c38f5f40
Nauris Pūķis <hi@pyronaur.com>
2022-04-06 11:07:24 +0300

    Update to Astro 1.0.0-beta.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,24 @@
 import astroRemark from '@astrojs/markdown-remark';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-export default /** @type {import('astro').AstroUserConfig} */ ({
-	markdownOptions: {
-
-		render: [
-			astroRemark,
-			{
+// https://astro.build/config
+export default defineConfig(
+	/** @type {import('astro').AstroUserConfig} */
+	{
+		markdown: {
+			render: [astroRemark, {
 				remarkPlugins: [],
-				rehypePlugins: [
-					'rehype-slug',
-					['rehype-autolink-headings', { behavior: 'wrap' }],
-				],
-
+				rehypePlugins: ['rehype-slug', ['rehype-autolink-headings', {
+					behavior: 'wrap'
+				}]],
 				shikiConfig: {
 					theme: 'poimandres',
 					langs: [],
-					wrap: false,
-				},
-			},
-		],
-	},
-	buildOptions: {
-		sitemap: true,
+					wrap: false
+				}
+			}]
+		},
 		site: 'http://localhost:3000/',
-	},
-});
+		integrations: [sitemap()]
+	});
--- src/data/post.ts
+++ src/data/post.ts
@@ -1,3 +1,4 @@
+type MarkdownInstance = import('astro').MarkdownInstance<any>;
 // Which mode is the environment running in? https://vitejs.dev/guide/env-and-mode.html#intellisense-for-typescript
 const { MODE } = import.meta.env;
 
@@ -12,31 +13,32 @@ export type Post = {
 }
 
 
-export function single(post): Post {
-	const slug = post.file.pathname.split('/').reverse()[0].replace('.md', '');
+export function single(post: MarkdownInstance): Post {
+	const slug = post.file.split('/').reverse()[0].replace('.md', '');
 	return {
-		...post,
+		...post.frontmatter,
+		Content: post.Content,
 		slug: slug,
-		draft: post.file.pathname.split('/').reverse()[1] === 'drafts',
-		timestamp: (new Date(post.date)).valueOf()
+		draft: post.file.split('/').reverse()[1] === 'drafts',
+		timestamp: (new Date(post.frontmatter.date)).valueOf()
 	}
 }
 
-export function published(posts: Post[]): Post[] {
+export function published(posts: MarkdownInstance[]): Post[] {
 	return posts
-		.filter(post => post.title )
+		.filter(post => post.frontmatter.title )
 		.map(post => single(post))
 		.filter(post => MODE === 'development' || !post.draft)
 		.sort((a, b) => b.timestamp - a.timestamp)
 }
 
-export function getRSS(posts: Post[]) {
+export function getRSS(posts: MarkdownInstance[]) {
 	return {
 		title: 'Simple Blog RSS',
 		description: 'Simple Blog RSS Feed',
 		stylesheet: true,
 		customData: `<language>en-us</language>`,
-		items: posts.map((post: Post) => ({
+		items: published(posts).map((post: Post) => ({
 			title: post.title,
 			description: post.preview,
 			link: post.slug,
